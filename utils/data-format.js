615d4efbebbb36e4625db02b44319d74a0e874cc
Adam Kelly <adamkelly2201@gmail.com>
2018-08-22 00:03:59 +0100

    Clean up javascript

--- utils/data-format.js
+++ utils/data-format.js
@@ -1,8 +1,10 @@
 // A small helper script to format the question data
 
-const fs = require('fs')
+const fs = require("fs");
 
-const data = fs.readdirSync('data')
-const questions = data.map(filename => JSON.parse(fs.readFileSync(`data/${filename}`)))
+const data = fs.readdirSync("data");
+const questions = data.map(filename =>
+  JSON.parse(fs.readFileSync(`data/${filename}`))
+);
 
-fs.writeFileSync('data.json', JSON.stringify(questions))
+fs.writeFileSync("data.json", JSON.stringify(questions));
--- utils/scrapper.js
+++ utils/scrapper.js
@@ -1,35 +1,35 @@
-const fs = require('fs')
-const Xray = require('x-ray')
+const fs = require("fs");
+const Xray = require("x-ray");
 const x = Xray();
 
-const BASE_URL = 'http://theory-tester.com/questions/'
+const BASE_URL = "http://theory-tester.com/questions/";
 
 const scrappers = {
-    question: '.pageHeading',
-    heading: '.p-questionSingle-heading',
-    explanation: '.p-questionSingle-explanation',
-    image: '.quiz-question-img > img:nth-child(1)@src',
-    questions: ['li.options-single > label:nth-child(1)'],
-    answer: 'li.options-single.js-correct-answer > label:nth-child(1)'
-}
+  question: ".pageHeading",
+  heading: ".p-questionSingle-heading",
+  explanation: ".p-questionSingle-explanation",
+  image: ".quiz-question-img > img:nth-child(1)@src",
+  questions: ["li.options-single > label:nth-child(1)"],
+  answer: "li.options-single.js-correct-answer > label:nth-child(1)"
+};
 
 // Scrape a single question given it's id
 function scrape(idx) {
-    return new Promise((resolve, reject) => {
-        x(BASE_URL + idx, scrappers)((err, data) => {
-            if (err) reject(err)
+  return new Promise((resolve, reject) => {
+    x(BASE_URL + idx, scrappers)((err, data) => {
+      if (err) reject(err);
 
-            if (!data.heading) {
-                resolve(null)
-            }
+      if (!data.heading) {
+        resolve(null);
+      }
 
-            data.heading = data.heading && data.heading.trim()
-            data.explanation = data.explanation && data.explanation.trim()
-            data.questions = data.questions && data.questions.map(x => x.trim())
-            data.answer = data.answer && data.answer.trim()
-            data.id = idx
+      data.heading = data.heading && data.heading.trim();
+      data.explanation = data.explanation && data.explanation.trim();
+      data.questions = data.questions && data.questions.map(x => x.trim());
+      data.answer = data.answer && data.answer.trim();
+      data.id = idx;
 
-            resolve(data)
-        })
-    })
+      resolve(data);
+    });
+  });
 }
